f449826fbe9e8f6fdc62bc21e3d66aa9a9a486da
Brandon J Wong <branjwong@gmail.com>
2018-05-11 16:14:47 -0700

    finish ch4

--- haskell/real-world-haskell/app/ch4/InteractWith.hs
+++ haskell/real-world-haskell/app/ch4/InteractWith.hs
@@ -2,3 +2,17 @@
 -- Save this in a source file, e.g. Interact.hs
 
 import System.Environment (getArgs)
+
+interactWith function inputFile outputFile = do
+    input <- readFile inputFile
+    writeFile outputFile (function input)
+
+main = do
+    args <- getArgs
+    case args of
+        [input,output] -> interactWith myFunction input output
+        _ -> putStrLn "error: exactly two arguments needed"
+
+myFunction :: String -> String
+myFunction inputString =
+    "no thanks"
